6e677709be8cfc85d6054136f515cf5872a71814
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-06-03 13:51:55 +0300

    simplify build script

--- crates/syntax/build.rs
+++ crates/syntax/build.rs
@@ -1,27 +1,11 @@
-use std::path::PathBuf;
-
-fn compile_parser(resources_dir: PathBuf) {
-    let move_parser_file =
-        std::fs::canonicalize(resources_dir.join("parser.c")).unwrap_or_else(|_| {
-            panic!(
-                "No such file or directory {:?}",
-                resources_dir
-                    .join("parser.c")
-                    .into_os_string()
-                    .into_string()
-                    .unwrap()
-            )
-        });
+fn main() {
+    let resources_dir = std::env::current_dir().unwrap().join("resources");
+    let move_parser_file = resources_dir.join("parser.c");
 
     println!("cargo:rerun-if-changed={}", resources_dir.to_str().unwrap()); // <1>
 
     cc::Build::new()
         .file(move_parser_file)
-        .include(resources_dir)
+        .include(&resources_dir)
         .compile("tree-sitter-move");
-}
-
-fn main() {
-    let resources_dir = std::env::current_dir().unwrap().join("resources");
-    compile_parser(resources_dir);
 }
